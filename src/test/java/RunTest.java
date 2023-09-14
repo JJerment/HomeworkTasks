eaebb3c2f5040fb80150a79569d30aa015d01ba1
{KNaumov} <{grezzy1995@yandex.ru}>
2023-09-14 16:36:14 +0300

    Error Test

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -10,17 +10,27 @@ public class RunTest {
     public void setUp() {
         System.out.println("Before test");
     }
+
     @After
     public void tearDown() {
         System.out.println("After test");
     }
+
     @Test
-    public void someTest(){
+    public void someTest() {
         open("https://demoqa.com/automation-practice-form");
         int a = 10;
         int b = 10;
         int c = 1;
-        int sum = a+b+c;
-        Assertions.assertTrue(sum==15);
+        int sum = a + b + c;
+        Assertions.assertTrue(sum == 21);
     }
-}
+
+    @Test
+    public void errorTest() {
+        int x = 5;
+        int y = 1;
+        int sum= x / y;
+        Assertions.assertTrue(sum == 10);
+        }
+    }
