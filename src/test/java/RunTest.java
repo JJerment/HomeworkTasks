c4271fcc52afe748187768215cb38afc24b753c3
{KNaumov} <{grezzy1995@yandex.ru}>
2023-09-14 16:22:04 +0300

    Added  Before&After

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -1,9 +1,19 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class RunTest {
+    @Before
+    public void setUp() {
+        System.out.println("Before test");
+    }
+    @After
+    public void tearDown() {
+        System.out.println("After test");
+    }
     @Test
     public void someTest(){
         open("https://demoqa.com/automation-practice-form");
